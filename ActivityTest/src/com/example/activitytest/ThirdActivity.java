989141acf968e811e834b8f95424cd986c6274ba
mossgreen <unfeifei@gmail.com>
2015-11-27 10:26:11 +1300

    bug fixed in project of ActivityTest

---
+++ ActivityTest/src/com/example/activitytest/ThirdActivity.java
@@ -0,0 +1,14 @@
+package com.example.activitytest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class ThirdActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.third_layout);
+	}
+}
