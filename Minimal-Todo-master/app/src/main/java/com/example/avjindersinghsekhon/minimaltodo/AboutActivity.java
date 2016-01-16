f1fd6eec10783045a75131c20414629c14878323
mossgreen <gufeifei@outlook.com>
2016-01-16 14:47:01 +1300

    add comments to AboutActivity.class

--- Minimal-Todo-master/app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/AboutActivity.java
+++ Minimal-Todo-master/app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/AboutActivity.java
@@ -30,6 +30,14 @@ public class AboutActivity extends AppCompatActivity {
         app = (AnalyticsApplication)getApplication();
         app.send(this);
 
+        /*
+        @moss
+        in the onCreate phase, app gets the theme,
+        if the theme is dark theme, just set the theme to dark
+        otherwise, set the light theme
+
+        then, set the color of the bakck arrow " <--" to white
+        */
         theme = getSharedPreferences(MainActivity.THEME_PREFERENCES, MODE_PRIVATE).getString(MainActivity.THEME_SAVED, MainActivity.LIGHTTHEME);
         if(theme.equals(MainActivity.DARKTHEME)){
             Log.d("OskarSchindler", "One");
@@ -52,6 +60,17 @@ public class AboutActivity extends AppCompatActivity {
 
         }
         try{
+
+            /*
+            @moss
+            通过PackageManager可以获取手机端已安装的apk文件的信息
+            PackageManager这个类是用来返回各种的关联了当前已装入设备了的应用的包的信息。你可以通过getPacageManager来得到这个类。 
+            通过 PackageInfo  获取具体信息方法： 
+            包名获取方法：packageInfo.packageName 
+            icon获取获取方法：packageManager.getApplicationIcon(applicationInfo) 
+            应用名称获取方法：packageManager.getApplicationLabel(applicationInfo) 
+            使用权限获取方法：packageManager.getPackageInfo packageName,PackageManager.GET_PERMISSIONS).requestedPermissions
+            */
             PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), 0);
             appVersion = info.versionName;
         }
@@ -60,6 +79,15 @@ public class AboutActivity extends AppCompatActivity {
         }
 
 
+        /*
+        @moss
+        first, find the TextView, which named mverisionTextview from R.id
+        second, set the text, which is from R.string, to this TextView 
+        third, find the Toolbar, which named toolbar, from R.id
+        fourth, find the TextView, which is named contactMe, from R.id
+
+        set an on click listener to the contactMe info, which will trigger a app.send method
+        */
         mVersionTextView = (TextView)findViewById(R.id.aboutVersionTextView);
         mVersionTextView.setText(String.format(getResources().getString(R.string.app_version), appVersion));
         toolbar = (Toolbar)findViewById(R.id.toolbar);
@@ -72,7 +100,15 @@ public class AboutActivity extends AppCompatActivity {
             }
         });
 
-
+        /*
+        @moss
+        关于使用toobar 的layout:
+        1. 在 activity_main.xml 里面添加 Toolbar 控件：
+        2.在 MainActivity.java 中加入 Toolbar 的声明：
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+        声明后，再将之用 setSupportActionBar 设定，Toolbar即能取代原本的 actionbar 了
+        */
         setSupportActionBar(toolbar);
         if(getSupportActionBar()!=null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -80,10 +116,22 @@ public class AboutActivity extends AppCompatActivity {
         }
     }
 
+    /*
+    @moss
+    this is the menu on the left top on the screen
+    click this will navigate user back to main activity
+    */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case android.R.id.home:
+
+                /*
+                NavUtils provides helper functionality for applications implementing recommended Android UI navigation patterns.
+                public static String getParentActivityName (Activity sourceActivity)
+                Return the fully qualified class name of sourceActivity's parent activity 
+                as specified by a PARENT_ACTIVITY <meta-data> element within the activity element in the application's manifest.
+                */
                 if(NavUtils.getParentActivityName(this)!=null){
                     NavUtils.navigateUpFromSameTask(this);
                 }
